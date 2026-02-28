530d6760b73ec953ce3040cd5135da55890361d0
maheshwariakshat34 <akshat.lakhotiya_cs.aiml24@gla.ac.in>
2026-02-28 11:07:35 +0530

    Configure RGB pins as output and initialize serial communication

--- src/main.cpp
+++ src/main.cpp
@@ -3,3 +3,13 @@
 int redPin   = 9;
 int greenPin = 10;
 int bluePin  = 11;
+
+void setup() {
+    Serial.begin(9600);
+
+    pinMode(redPin, OUTPUT);
+    pinMode(greenPin, OUTPUT);
+    pinMode(bluePin, OUTPUT);
+
+    Serial.println("RGB System Initialized");
+}
